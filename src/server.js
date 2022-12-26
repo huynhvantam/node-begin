fd8ea7afc98ea0c972753f67bc53cbb2e4f66f89
huynhvantam <53110142+huynhvantam@users.noreply.github.com>
2022-12-26 11:02:47 +0700

    31

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
+
 
 const app = express()
 const port = process.env.PORT || 8888
@@ -10,6 +12,16 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 //khai bao route
 app.use('/', webRoutes)
+
+//test connetion
+
+
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(results)
+    }
+)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
